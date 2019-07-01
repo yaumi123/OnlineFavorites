b5a4bafc530c9ae6183126ace470f5b03a76b6b6
feifei <87603528@qq.com>
2019-07-02 05:01:09 +0800

    create project

--- src/main/java/com/ares/OnlineFavorites/controller/AddFavoriteController.java
+++ src/main/java/com/ares/OnlineFavorites/controller/AddFavoriteController.java
@@ -1,4 +1,21 @@
 package com.ares.OnlineFavorites.controller;
 
+import com.ares.OnlineFavorites.api.request.AddFavoriteRequest;
+import com.ares.OnlineFavorites.api.response.ListFavoritesResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+@CrossOrigin("*")
 public class AddFavoriteController {
+
+ /*   @PostMapping("/favorite")
+    public ResponseEntity<?> addFavorite (@RequestBody AddFavoriteRequest addFavoriteRequest){
+
+    }*/
+
+
 }
